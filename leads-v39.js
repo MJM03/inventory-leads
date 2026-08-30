49ff890b4b3f1ceae95efac0354d80dac52c1f7a
MJM03 <mjm030798@gmail.com>
2026-08-29 22:39:18 -0500

    Add 150 researched Lima and Callao prospects

---
+++ leads-v39.js
@@ -0,0 +1,153 @@
+(()=>{const rows=`Ferretería Macuri|Santiago de Surco
+Ferretería Elizabeth|Ate
+Ferretería Olivares|Ate
+Ferretería Roncalli|Surquillo
+Ferretería Torres Santa Anita|Santa Anita
+Ferretería Pinotello|Santa Anita
+Ferretería San Roque|Santa Anita
+Ferretería Depósito y Casa|La Molina
+Ferretería Chalito|Cieneguilla
+Ferretería R.I.O.|Pachacámac
+Ferretería Yeni|Pachacámac
+Ferretería Villa Hermosa|Pachacámac
+Ferretería La Base|Pachacámac
+Ferretería Las Colinas|Pachacámac
+Ferretería Dimacom|Santa Anita
+Ferretería Mayhua|Ate
+Ferretería Taco|Cieneguilla
+Ferretería Femeconst|Santa Anita
+Ferretería Techitos|Pachacámac
+Ferretería Punto Ferretero|El Agustino
+Ferretería Vilma|El Agustino
+Ferretería Kgari Atik SRL|El Agustino
+Ferretería Dinfeco|El Agustino
+Ferretería Hortega|El Agustino
+Ferretería Santa Rosa El Agustino|El Agustino
+Ferretería Nancy|Pachacámac
+Ferretería Vasquez|El Agustino
+Ferretería y Distribuidora Nuestra Señora de la Merced|El Agustino
+Ferretería Richards|Surquillo
+Ferretería Constrans SAC|Surquillo
+Ferretería Ferrematco|San Luis
+Ferretería Luciano|Ate
+Ferretería Miguel Ángel|Ate
+Ferretería Multiservicios Q.H|El Agustino
+Ferretería Milagros Ate|Ate
+Inversiones MHM|Cieneguilla
+Comercial Ferretero J&M|Pachacámac
+Ferretería Cromat|La Molina
+Ferretería J&M Santa Anita|Santa Anita
+Ferretería Manolito (Mariy 2)|Cieneguilla
+Representaciones Noriega Vasquez|El Agustino
+Ferretería San Antonio Pachacámac|Pachacámac
+Ferretería Liz|Ate
+Ferretería Pilar|Ate
+Ferretería Santa Rosa Ate|Ate
+Ferretería Correa|Ate
+Ferretería Llerena|Lurigancho
+Ferretería El Niño Jesús|Chaclacayo
+Ferretería Corazón de Jesús Chaclacayo|Chaclacayo
+Ferretería Nicol|Ate
+Ferretería Malvinitas|Ate
+Ferretería Reyes|Lurigancho
+Ferretería Luna Ate|Ate
+Ferretería Chiky|Ate
+Ferretería Nicolas|Ate
+Phoenix Materials E.I.R.L.|Lurigancho
+Ferretería El Bosque|Chaclacayo
+Ferretería Inversiones Marcos|Ate
+Ferretería Manantial|Lurigancho
+Ferretería Unión de Pariachi|Ate
+Ferretería Bermudo|Ate
+Ferretería El Gringo|Ate
+Ferretería Muruhuay|Ate
+Ferretería R y Z|Ate
+Ferretería Correa 2|Ate
+Ferretería Coco Perez|Lurigancho
+Ferretería Kiara|Huaycán
+Ferretería Deivis|Ate
+Ferretería Santa Clara|Ate
+Ferretería M & N Negritos|Lurigancho
+Ferretería Mileny|Ate
+Ferretería Sander|Ate
+Ferretería Wilder|Lurigancho - Chosica
+Ferretería Danmer|San Juan de Lurigancho
+Ferretería Las Delicias|San Juan de Lurigancho
+Ferretería Huaripoma|San Juan de Lurigancho
+Ferretería Los Andes|San Juan de Lurigancho
+Ferretería Cofeco|San Juan de Lurigancho
+Ferretería El Sol|San Juan de Lurigancho
+Ferretería Alva|San Juan de Lurigancho
+Ferretería Jokap|San Juan de Lurigancho
+Ferretería La Loza|San Juan de Lurigancho
+Ferretería Jhon|San Juan de Lurigancho
+Ferretería Willy|San Juan de Lurigancho
+Ferretería Chavez|San Juan de Lurigancho
+Ferretería Machupichu|San Juan de Lurigancho
+Ferretería Dnilson|San Juan de Lurigancho
+Ferretería Herrera|San Juan de Lurigancho
+Ferretería Jerusalen|San Juan de Lurigancho
+Ferretería Milano|San Juan de Lurigancho
+Ferretería Las Gemelas|San Juan de Lurigancho
+Ferretería-Depósito M y R|San Juan de Lurigancho
+Ferretería Ederick|Lurigancho
+Ferretería Ticse|Lurigancho
+Ferretería Emanuel|San Juan de Lurigancho
+Ferretería Jhire|San Juan de Lurigancho
+Ferretería Vigaproductos SAC|San Juan de Lurigancho
+Ferretería Danubio Azul|San Juan de Lurigancho
+Ferretería Pino|San Juan de Lurigancho
+Ferretería Riveros|San Juan de Lurigancho
+Ferretería Isabel|San Juan de Lurigancho
+Ferretería Torres Lurigancho|Lurigancho
+Ferretería Jashe|San Juan de Lurigancho
+Ferretería Dios Habla Hoy|Lurigancho
+Ferretería Jeniven|San Juan de Lurigancho
+Ferretería Mejia I|San Juan de Lurigancho
+Ferretería Inca|San Juan de Lurigancho
+Ferretería San Cristobal|San Juan de Lurigancho
+Ferretería Andino M y S|San Juan de Lurigancho
+Ferretería El Chino|San Juan de Lurigancho
+Ferretería Huanca|San Juan de Lurigancho
+Ferretería Diamante|San Juan de Lurigancho
+Ferretería San Hilarion|San Juan de Lurigancho
+Ferretería Casuarinas|San Juan de Lurigancho
+Ferretería TH|San Juan de Lurigancho
+Ferretería Krisel|San Juan de Lurigancho
+Ferretería Criscemi|San Juan de Lurigancho
+Ferretería Inversiones Rea|San Juan de Lurigancho
+Ferretería Oxapampa|San Juan de Lurigancho
+Ferretería Villafuerte|San Juan de Lurigancho
+Ferretería Shalom|San Juan de Lurigancho
+Inversiones Max SAC|San Juan de Lurigancho
+Ferretería Juan Pablo|San Juan de Lurigancho
+Ferretería Inversiones Fierro Sur E.I.R.L|San Juan de Lurigancho
+Ferretería Espinoza|San Juan de Lurigancho
+Ferretería Samira|Lurigancho
+Progresol Ponce|Carabayllo
+Progresol Charito II|Carabayllo
+Progresol Amazonas|Carabayllo
+Progresol Miraflores|Carabayllo
+Progresol Santa Monica|Comas
+Progresol Huascaran|Comas
+Fedemarr E.I.R.LTDA|Comas
+Progresol El Pinar|Comas
+Progresol Camila|Comas
+Ferretería Corazón de Jesús Comas|Comas
+Progresol Charito I|Comas
+Progresol M y N|Comas
+Ferretería Castillo|Independencia
+Ferretería San Gabriel|Callao - Oquendo
+Progresol Portales & Vicky|Callao - Oquendo
+Ferretería Pilares|Callao
+Ferretería Castro Callao|Callao
+Ferretería Hernan|Callao
+Ferretería Danuska|Callao
+Ferretería Gutierrez|San Martín de Porres
+Progresol Casa Blanca|San Martín de Porres
+Progresol Andy|San Martín de Porres
+Progresol Leyva|San Martín de Porres
+Progresol Torres|San Martín de Porres
+Ferretería Quito|San Martín de Porres
+Progresol Migal|San Martín de Porres
+Ferretería Zarita|San Martín de Porres`.trim().split('\n');const slug=s=>s.normalize('NFD').replace(/[\u0300-\u036f]/g,'').toLowerCase().replace(/[^a-z0-9]+/g,'-').replace(/^-|-$/g,'');const extra=rows.map((r,i)=>{const [company,district]=r.split('|');return{id:`v39-${i+1}-${slug(company)}`,company,sector:'Ferretería / materiales de construcción',district,score:78,closeScore:84,phone:'',whatsapp:'',email:'',web:'',address:'',source:'Red pública de ferreterías UNACEM · contacto por enriquecer'}});const existing=new Set((window.INVENTORY_LEADS||[]).map(x=>x.company.toLowerCase().trim()));window.INVENTORY_LEADS=(window.INVENTORY_LEADS||[]).concat(extra.filter(x=>!existing.has(x.company.toLowerCase().trim())).slice(0,150));})();
